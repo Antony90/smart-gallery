a9adf9225bdd60861122442be73992940fb5f5d1
Antony90 <antony.antoniou18@gmail.com>
2023-08-16 22:51:02 +0100

    add test data

---
+++ src/data/faces.ts
@@ -0,0 +1,34 @@
+import { People, Person } from "../models/Person"
+import { photos, photosArr } from "./photos"
+
+
+const musk: Person = {
+    id: "1234",
+    name: "Elon Musk",
+    photoIDs: Object.entries(photos).filter(([_, p]) => p.name.toLowerCase().includes("musk")).map(([id, p]) => id)
+} 
+
+const rock: Person = {
+    id: "4321",
+    name: "Dwayne \"The Rock\" Johnson",
+    photoIDs: Object.entries(photos).filter(([_, p]) => p.name.toLowerCase().includes("rock")).map(([id, p]) => id)
+} 
+
+const keanu: Person = {
+    id: "2112",
+    name: "Keanu Reeves",
+    photoIDs: Object.entries(photos).filter(([_, p]) => p.name.toLowerCase().includes("keanu")).map(([id, p]) => id)
+} 
+const mark: Person = {
+    id: "2112",
+    name: "Mark Zuckerberg",
+    photoIDs: Object.entries(photos).filter(([_, p]) => p.name.toLowerCase().includes("mark")).map(([id, p]) => id)
+} 
+
+export const faces = {
+    "11": rock,
+    "1": musk,
+    "1111": mark,
+    "111": keanu,
+} as People;
+
