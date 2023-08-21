7076ccfc9e0f8c2a373e6709ef42915d31765633
Antony90 <antony.antoniou18@gmail.com>
2023-08-21 14:16:50 +0100

    header search box, icons, breadcrumb

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
 import './Layout.css';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
-import { Layout as ADLayout, Breadcrumb, Menu, Typography, Space, FloatButton, theme } from 'antd';
-import { BarChartOutlined, CodepenOutlined, DingtalkOutlined, FolderOpenOutlined, HomeOutlined, NotificationOutlined, PictureOutlined, RobotOutlined, TeamOutlined } from '@ant-design/icons';
+import { Layout as ADLayout, Breadcrumb, Menu, Typography, Space, FloatButton, theme, Input, Row, Col, Avatar, Button, Badge, Select } from 'antd';
+import { BarChartOutlined, BellFilled, CodepenOutlined, DingtalkOutlined, FolderOpenOutlined, HomeFilled, HomeOutlined, NotificationFilled, NotificationOutlined, PictureOutlined, RobotOutlined, SettingFilled, TeamOutlined, UserOutlined } from '@ant-design/icons';
 import { blue } from '@ant-design/colors';
 import { useState } from 'react';
+import { SettingsOutlined } from '@mui/icons-material';
 
 
 const { Header, Content, Footer, Sider } = ADLayout;
 const { Title, Text } = Typography;
+const { Search } = Input;
 
 const menuItems = [
   {
@@ -41,7 +43,7 @@ const menuItems = [
     key: '/albums',
     icon: <FolderOpenOutlined />
   }
-]
+];
 
 const Layout: React.FC = () => {
   const navigate = useNavigate();
@@ -56,14 +58,26 @@ const Layout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <ADLayout style={{ minHeight: '100vh' }}>
-      <Sider width={214} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="logo" >
-          <CodepenOutlined className="logo-icon" style={{ fontSize: (collapsed ?  35: 28), marginLeft: collapsed ? 8: 0 }} />
-          {!collapsed && <Title 
-            level={3} 
-            className="logo-title"
-          >Smart Gallery</Title>}
+    <ADLayout style={{ minHeight: "100vh" }}>
+      <Sider
+        width={214}
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className="logo">
+          <CodepenOutlined
+            className="logo-icon"
+            style={{
+              fontSize: collapsed ? 35 : 28,
+              marginLeft: collapsed ? 8 : 0,
+            }}
+          />
+          {!collapsed && (
+            <Title level={3} className="logo-title">
+              Smart Gallery
+            </Title>
+          )}
         </div>
 
         <Menu
@@ -74,20 +88,75 @@ const Layout: React.FC = () => {
           selectedKeys={[selected]}
           onClick={(e) => {
             setSelected(e.key);
-            navigate(e.key)
+            navigate(e.key);
           }}
         />
       </Sider>
       <ADLayout>
-        <Header style={{ padding: 0, background: "white" }} />
-        <Content style={{ margin: '20px 20px' }}>
+        <Header
+          style={{
+            background: "white",
+            padding: 16,
+            paddingLeft: 30,
+            paddingRight: 30,
+            height: 73,
+          }}
+        >
+          <Row gutter={[24, 0]}>
+            <Col span={9}>
+              <div style={{ marginTop: 10 }}>
+                <Breadcrumb
+                  items={[
+                    {
+                      href: "/photos",
+                      title: <HomeFilled/>,
+                    },
+                    {
+                      title: "Photos",
+                    },
+                  ]}
+                />
+
+              </div>
+            </Col>
+            <Col span={6}>
+              <Search
+                placeholder="Type here"
+                allowClear
+                enterButton
+                size="large"
+                addonBefore={
+                  <Select defaultValue="all">
+                    <Select.Option value="all">All</Select.Option>
+                    <Select.Option value="photos">Photos</Select.Option>
+                    <Select.Option value="faces">Faces</Select.Option>
+                    <Select.Option value="albums">Albums</Select.Option>
+                  </Select>
+                }
+                suffix={<Text keyboard>Ctrl /</Text>}
+              />
+            </Col>
+            <Col span={9}>
+              <div className="header-buttons">
+                <Avatar icon={<UserOutlined />} size="large" />
+                <Button type="default" icon={<SettingFilled />} size="large" />
+                <Badge count={5}>
+                  <Button type="default" icon={<BellFilled />} size="large" />
+                </Badge>
+              </div>
+            </Col>
+          </Row>
+        </Header>
+        <Content style={{ margin: "20px 20px" }}>
           <Outlet />
           <FloatButton.BackTop style={{ left: 24 }} />
         </Content>
-        <Footer style={{ textAlign: 'center' }}>yentio | using <a href="https://ant.design">ant.design</a></Footer>
+        <Footer style={{ textAlign: "center" }}>
+          yentio | using <a href="https://ant.design">ant.design</a>
+        </Footer>
       </ADLayout>
     </ADLayout>
-  )
+  );
 }
 
 export default Layout
